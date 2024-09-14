1a5639513b00424a12fad9e30a92aac6017f6319
naggne <gosari05@gmail.com>
2024-09-15 03:32:22 +0900

    EP04 commit

---
+++ pythonbasic/EP04_DATATYPE.py
@@ -0,0 +1,50 @@
+#정수_소수점없는 - int
+from operator import truediv
+
+age = 20
+print(age)
+print(type(age))
+
+#실수_소수점있는 - float
+
+weight = 80.5
+print(weight)
+print(type(weight))
+
+#문자열 - str
+
+greeting = "hello world"
+print(greeting)
+print(type(greeting))
+
+#불리언 - bool
+
+is_adult = True
+print(is_adult)
+print(type(is_adult))
+
+#----------
+# 파이썬의 자료구조면서 데이터타입인
+
+#리스트 - list 나열
+fruits = ['사과', '바나나','딸기']
+print(fruits)
+print(type(fruits))
+
+# 튜플 - 변하지 않는 값을 만들때 소괄호
+location = ('37.123','58.124')
+print(location)
+print(type(location))
+
+ # 딕셔너리 - dic
+student_scores={'철수' : 90, '영희' : 45, '민수' : 88}
+print(student_scores)
+print(type(student_scores))
+
+# 세트 - set 중복된값은 빼고 알려줌
+colors = {'red','red','blue','black'}
+print(colors)
+print(type(colors))
+
+
+
