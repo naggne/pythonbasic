82c318ca61bd32dbe2503de7a86f155a0702d9fd
naggne <gosari05@gmail.com>
2024-09-16 22:54:07 +0900

    EP06 commit

--- pythonbasic/EP05_if.py
+++ pythonbasic/EP05_if.py
@@ -1,8 +1,12 @@
-age = 18
+age = 10
 
 if age >= 19:
     print("성인입니다")
+elif 16 <= age <= 18:
+    print("고등학생입니다")
+elif 13 <= age<= 15:
+    print("중학생입니다")
 else:
-    print("미성년자입니다")
+    print("중학생 이하의 어린이들")
 
 
---
+++ pythonbasic/EP06_while.py
@@ -0,0 +1,21 @@
+
+#range 함수 for문
+# for num in range(1,101) :
+#     print(num)
+#
+# print("end")
+
+# num = 1
+#
+# while num <= 100:
+#     print(num)
+#     num = num + 1
+# print("end")
+
+num = 1
+while True:
+    print(num)
+    num = num + 1
+    if num >= 101:
+        break
+print("end")
